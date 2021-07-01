0b9f4d36741db9cfeb0cdc77efdda13ddd128c24
despencer <despencer@mail.ru>
2021-07-01 11:48:50 +0300

    Tests set up.

--- tests.py
+++ tests.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
+import json
 import unittest
 
+class TreexTest(unittest.TestCase):
+    def domirror(self, testdef):
+        self.assertTrue(True)
+
 if __name__ == '__main__':
-    unittest.main()
+    with open('tests.json') as jtests:
+        tests = json.load(jtests)
+        for t in tests:
+            setattr(TreexTest, "test"+t["name"], lambda s: getattr(TreexTest, "do"+t["type"])(s, t))
+    unittest.main(verbosity=2)
