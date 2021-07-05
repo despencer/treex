020a0c5335e62c11f30ab62f055c3e701de6e53c
despencer <despencer@mail.ru>
2021-07-05 23:17:11 +0300

    Rewrited for own test runner and wiped out standard unit tests.

--- tests.py
+++ tests.py
@@ -2,9 +2,12 @@
 
 import json
 from treex import Treex
-import unittest
 
-class TreexTest(unittest.TestCase):
+class TreexTest():
+    def __init__(self):
+        self.good = 0
+        self.fail = 0
+
     def domirror(self, testdef):
         source = Treex.fromjson(testdef["source"])
         self.assertTrue(Treex.match(source, source))
@@ -14,9 +17,27 @@ class TreexTest(unittest.TestCase):
         target = Treex.fromjson(testdef["target"])
         self.assertFalse(Treex.match(source, target))
 
+    def run(self, tests):
+        for t in tests:
+            if ("active" not in t) or (t["active"]):
+                func = getattr(TreexTest, "do"+t["type"])
+                self.current = t
+                print("Running {0}".format(t["name"] ))
+                func(self, t)
+        print("Total good: {0}, failed: {1}".format(self.good, self.failed))
+
+    def assertTrue(self, value):
+        if value:
+            self.fail = self.fail + 1
+            print("Test {0} failed".format(self.current["name"]))
+        else:
+            self.good = self.good + 1
+
+    def assertFalse(self, value):
+        assertTrue(not value)
+
 if __name__ == '__main__':
     with open('tests.json') as jtests:
         tests = json.load(jtests)
-        for t in tests:
-            setattr(TreexTest, "test"+t["name"], lambda s: getattr(TreexTest, "do"+t["type"])(s, t))
-    unittest.main(verbosity=2)
+    runner = TreexTest()
+    runner.run(tests)
